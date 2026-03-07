57a3ba709507e182e54d81a01873a61e0bd8e7c9
linuswong <linuswong9@gmail.com>
2026-03-06 16:38:57 -0800

    functors for comparing orders

--- backend/stock.h
+++ backend/stock.h
@@ -3,14 +3,30 @@
 #include <queue>
 #include "order.h"
 using namespace std;
+
+struct BuyOrderCmp {
+    bool operator()(const Order* a, const Order* b) const {
+        if (a->price == b->price) return a->timestamp > b->timestamp; 
+        return a->price < b->price; 
+    }
+};
+
+struct SellOrderCmp {
+    bool operator()(const Order* a, const Order* b) const {
+        if (a->price == b->price) return a->timestamp > b->timestamp; 
+        return a->price > b->price; 
+    }
+};
 class Stock {
     private:
         string ticker;
-        priority_queue<Order*, vector<Order*>, greater<Order*>> buyOrders;
-        priority_queue<Order*, vector<Order*>, less<Order*>> sellOrders;
+        priority_queue<Order*, vector<Order*>, BuyOrderCmp> buyOrders;
+        priority_queue<Order*, vector<Order*>, SellOrderCmp> sellOrders;
+        double lastTradedPrice;
     public:
         Stock(string ticker);
         string getTicker();
+        double getLastTradedPrice();
         void placeBuyOrder(Order* order);
         void placeSellOrder(Order* order);
         void executeOrder(Order* order);
